ab63fbc38b67a13d8b37c989fc31d2f308dbba82
Tiana <Tiana.marie013@gmail.com>
2021-10-11 19:33:32 -0600

    relearning

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import body from './components/body';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        Hello World
       </header>
+      <body/>
     </div>
   );
 }
